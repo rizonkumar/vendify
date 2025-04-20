f2770edd7c678ad6ad11f06e98887f1b7b771880
Rizon Kumar Rahi <rizon.kumar.rahi@gmail.com>
2025-04-20 16:26:27 +0530

    Feat: Added Categories payload

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -5,12 +5,10 @@ export default async function Home() {
   const payload = await getPayload({
     config: configPromise,
   });
-  console.log("payload from the page", payload);
 
   const data = await payload.find({
     collection: "users",
   });
-  console.log("data", data);
 
   return <div className="">{JSON.stringify(data, null, 2)}</div>;
 }
